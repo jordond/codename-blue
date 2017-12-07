6e53b5fd9e7104975e61b377262dbc51cce28243
Jordon de Hoog <jordon.dehoog@gmail.com>
2017-12-06 19:26:54 -0500

    Initial Commit

---
+++ src/lang/ru.js
@@ -0,0 +1,13 @@
+﻿/* eslint-disable */
+
+const ru = {
+    headings: {
+        contact: "Контакты",
+        experience: "Опыт",
+        education: "Образование",
+        skills: "Навыки",
+        about: "Обо мне"
+    }
+};
+
+export default ru;
